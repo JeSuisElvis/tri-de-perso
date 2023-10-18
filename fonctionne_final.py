65c3be832bd3048704b55677a5c3fc51ef7b94c8
JeSuisElvis <148223176+JeSuisElvis@users.noreply.github.com>
2023-10-18 08:22:52 +0200

    Add files via upload

---
+++ fonctionne_final.py
@@ -0,0 +1,29 @@
+import csv
+# Ouverture du fichier CSV et création du dictionnaire
+tableau = {}
+with open('tour_de_jeu.csv', newline='') as f:
+    lire = csv.reader(f)
+    for ligne in lire:
+        # La première colonne du CSV est le nom du personnage, la deuxième est sa vitesse,
+        # et la troisième est son seuil d'apparition
+        nom_personnage = ligne[0]
+        vitesse = int(ligne[1])
+        seuil = int(ligne[2])
+        tableau[nom_personnage] = (vitesse, seuil)
+
+nb_iter = 100  # Nombre d'apparitions (base 100)
+c = 1
+s = ""
+max_val = max([v[0] for v in tableau.values()])  # Distance d'un tour (vitesse maximale parmi tous les personnages)
+while c <= nb_iter:
+    for key, value in tableau.items():
+        dist = int(c * value[0])
+        if dist % max_val <= value[1]:
+            s += key + ', ' # Nous notons son nom à chaque fois qu'il doit jouer
+            if value[0] == min([v[0] for v in tableau.values()]):  # Si le personnage actuel est le plus lent
+                s += '\n'  # Ajoute un retour à la ligne après chaque tour du personnage le plus lent
+        value = (value[0], dist % max_val)  # Nous notons la fréquence d'apparition depuis le début
+        tableau[key] = value  # et nous mettons à jour
+    c += 1
+
+print(s)
